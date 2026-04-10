47614e78a0a30de1ff0d059e6e3829f8830a22e9
anu29 <62901562+prabishkhadka@users.noreply.github.com>
2026-04-10 14:51:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+from src.pre_processing import Preprocessing
+
+def main():
+    print("Wildfire Burn Severity Pipeline\n")
+
+    print("Run input notebook first:")
+    print("notebooks/01_get_input.ipynb\n")
+
+    print("Preprocessing module ready.")
+
+if __name__ == "__main__":
+    main()
